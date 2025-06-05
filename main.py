cffeab16a5183201d77496b633a7ceb1f6662380
Jesus Abundis <elabundis@gmail.com>
2025-06-05 13:00:28 -0700

    Allow to track students across generations
    Also:
    - Fix missing functionality to add records to a Student
    - Provide verbose output if requested only

--- main.py
+++ main.py
@@ -32,9 +32,11 @@ class Hist(object):
 
 class Student(object):
     def __init__(self, name, hist) -> None:
-        "The student code can be zero or one"
         self.name = name
         self.hist = hist
+    def add_history(self, history):
+        new_records = history.records
+        self.hist.records.extend(new_records)
 
 def order_record(record):
     id, semester = record
@@ -57,7 +59,7 @@ def load_database():
         year += 1
     return generation
 
-def investigate_these(df, year, semester):
+def investigate(df, year, semester, verbose=False):
     "Returns list of students from desired semester whom to investigate"
     col = semester - 1  # transform to python index
     students = df.iloc[:, col]
@@ -78,15 +80,16 @@ def investigate_these(df, year, semester):
             # history.add_generation(year, (id[idx], sem[idx]))
             history.add_generation(year, record)
             pupils.append(Student(student, history))
-            print(student)
-            print(record)
-            # Notify if a student finished his/her bachelor's
-            if(record[1][-1] == 9):
-                print("Finished bachelor's")
-            print()
+            if(verbose):
+                print(student)
+                print(record)
+                # Notify if a student finished his/her bachelor's
+                if(record[1][-1] == 9):
+                    print("Finished bachelor's")
+                print()
     return pupils
 
-def search_across_gens(name, init_gen, database):
+def search_across_gens(name, init_gen, database, verbose=False):
     """
     Returns a Hist object with the records of student with given 'name'
     starting at desired generation 'init_gen' given the generations 'database'
@@ -98,10 +101,26 @@ def search_across_gens(name, init_gen, database):
             record = np.where(df == name)
             if(len(record[0]) != 0):
                 record = order_record(record)
-                print(year)
-                print(record)
+                if(verbose):
+                    print(year)
+                    print(record)
                 histories.add_generation(year, record)
     return histories
+
+def investigate_all(semester, init_gen, database):
+    """
+    Returns list of students from desired semester whom to investigate across
+    generations starting from 'init_gen'
+    """
+    # Students that require checking with history for initial generation
+    students = investigate(database[str(init_gen)], init_gen, semester)
+    # Add their histories in remaining generations
+    for student in students:
+        name = student.name
+        histories = search_across_gens(name, init_gen+1, database)
+        student.add_history(histories)
+    return students
+
 
 # Load student generations dataframe
 gen  = load_database()
